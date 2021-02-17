87cc7c9a818596c2b386ab8fc78264c3639af5ab
Lorenzo-Arnau <skyline03@hotmail.it>
2021-02-17 10:45:34 +0100

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,5 @@ let mix = require('laravel-mix');
 
 mix
 .sass('src/app.scss', 'dist/')
-.sass('src/app-bonus.scss', 'dist/');
+.sass('src/app-bonus.scss', 'dist/').options({ processCssUrls: false });
+;
